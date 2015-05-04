c9350f8cbeab4663a6c09ddd88358e30a46f01e0
Ramon Gebben <ramon@ra-ge.net>
2015-05-05 00:21:53 +0200

    Created a meerkat with css to test out the layers

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var app = express();
 
-app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
 
 app.get('/', function( req, res ){
 	res.render('index');
