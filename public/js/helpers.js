f89fd3553666c6d92578acd619b7599c63465f41
Ramon Gebben <ramon@ra-ge.net>
2015-06-21 15:46:17 +0200

    updated color_map for dynamic colors

--- public/js/helpers.js
+++ public/js/helpers.js
@@ -71,9 +71,9 @@ var calculateAspectRatioFit = function(srcWidth, srcHeight, maxWidth, maxHeight)
 }
 
 var percentColors = [
-    { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0 } },
-    { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0 } },
-    { pct: 1.0, color: { r: 0x00, g: 0xff, b: 0 } } ];
+    { pct: 0.0, color: { r: 0xff, g: 0x00, b: 0x00 } },
+    { pct: 0.5, color: { r: 0xff, g: 0xff, b: 0x00 } },
+    { pct: 1.0, color: { r: 0x00, g: 0xff, b: 0x00 } } ];
 
 var getColorForPercentage = function(pct) {
     for (var i = 1; i < percentColors.length - 1; i++) {
